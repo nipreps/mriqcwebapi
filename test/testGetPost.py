d68d117dfc6857a79c7f7178a2610f422b9af982
fhuang <wshfywpsa@126.com>
2017-05-04 12:28:47 -0400

    modify test in testGetPost.py

--- test/testGetPost.py
+++ test/testGetPost.py
@@ -4,13 +4,15 @@ from glob import glob
 # test data directory
 pattern = os.path.join('derivatives/', '*.json')
 
-def getRequest(postResponse, url):
-    # url for GET
+# url for GET
+def getURL(postResponse, url):
     dirID = postResponse.json()["_id"]
-    getURL = url + "/" + dirID
-    # getURL = url + "/" + "haha"
+    resURL = url + "/" + dirID
+    return resURL  
+
+def getRequest(postResponse, url):
     # GET
-    getResponse = requests.get(getURL)
+    getResponse = requests.get(getURL(postResponse, url))
     return getResponse.json()
 
 ###### MAIN ######
@@ -18,11 +20,27 @@ header = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
 class TestCase(unittest.TestCase):
 
-    def testDataValid(self):
+    def test_00_ConnectionStatus(self):
+         # 1. initialize param. change to others for later use
+        url = "http://localhost:80/scenarios"
+        
+        for fileName in glob(pattern):
+            print fileName
+            with open(fileName) as fp:
+                inputData = json.load(fp) 
+                # POST request
+                postResponse = requests.post(url, data = json.dumps(inputData), headers = header)
+                self.assertTrue( postResponse.raise_for_status() == None )
+                # GET request
+                getResponse = requests.get( getURL(postResponse, url) )
+                self.assertTrue( getResponse.raise_for_status() == None )
+
+    def test_01_DataValid(self):
         # 1. initialize param. change to others for later use
         url = "http://localhost:80/scenarios"
         
         for fileName in glob(pattern):
+            print("data " + fileName)
             with open(fileName) as fp:
                 inputData = json.load(fp) 
                 # 2. POST request
@@ -37,18 +55,6 @@ class TestCase(unittest.TestCase):
                     self.assertTrue(key in queriedData)
                     # check key-value pair match
                     self.assertTrue( inputData[key] == queriedData[key] )
-
-    def testForFailReport(self):
-         # 1. initialize param. change to others for later use
-        url = "http://localhost:80/scenarios"
-        
-        for fileName in glob(pattern):
-            with open(fileName) as fp:
-                inputData = json.load(fp) 
-                # POST request
-                postResponse = requests.post(url, data = json.dumps(inputData), headers = header)
-                # this should be false. intent to fail the test
-                self.assertTrue( postResponse.status_code == 200 )
 
 
 if __name__ == '__main__':
