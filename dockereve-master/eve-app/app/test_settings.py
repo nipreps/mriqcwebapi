95b632a7542526c5b88a2303b91da5d0f0fce497
oesteban <code@oscaresteban.es>
2017-06-01 18:27:20 -0700

    add eve to cache, some optimizations on dockerfile

--- dockereve-master/eve-app/app/test_settings.py
+++ dockereve-master/eve-app/app/test_settings.py
@@ -3,7 +3,6 @@ import os
 import re
 from flask_pymongo import MongoClient
 
-
 get_mongo_host = re.match('tcp://(.*):(.*)', os.environ['MONGODB_PORT'])
 url = 'scenarios'
 MONGO_HOST = get_mongo_host.group(1),
@@ -11,7 +10,7 @@ MONGO_PORT = int(get_mongo_host.group(2))
 MONGO_DBNAME = 'test_DB'
 class settingsTestCase(TestMinimal):
 	def setUp(self):
-		return super().setUp(settings_file = './settings.py') 
+		return super().setUp(settings_file = './settings.py')
 	def dropDB(self):
 		self.connection = MongoClient(MONGO_HOST,MONGO_PORT)
 		self.connection.drop_database(MONGO_DBNAME)
@@ -27,7 +26,7 @@ class settingsTestCase(TestMinimal):
 	 	self.assertFalse(self.domain)
 	 	self.assertFalse(return_json)
 	 	# self.assert200(return_code)
-	
+
 # class settingsTestCase(TestMinimal):
 # 	def dropDB(self):
 # 		self.connection = MongoClient(MONGO_HOST, MONGO_PORT)
@@ -38,7 +37,7 @@ class settingsTestCase(TestMinimal):
 # 		self.connection = MongoClient(MONGO_HOST, MONGO_PORT)
 # 		self.connection.drop_database(MONGO_DBNAME)
 # 	def testPost():
-#     	return_json, return_code = self.post(url,{"cjg":0.123123}) 
+#     	return_json, return_code = self.post(url,{"cjg":0.123123})
 # 		print(return_json)
 # 		print(return_code)
 
